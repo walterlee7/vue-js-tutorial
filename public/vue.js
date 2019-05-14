ab2f92551724885709e4830f7830f9510d8af2c6
walterlee <walter.lee9@gmail.com>
2019-05-14 15:21:16 -0500

    code display added

---
+++ public/vue.js
@@ -0,0 +1,103 @@
+import Vue from 'vue';
+
+new Vue({
+    el: '#app-0',
+    data: {
+        title: "Declarative Rendering: ",
+        message: 'Hello Vue!'
+    }
+})
+
+new Vue({
+    el: '#app-1',
+    data: {
+        title: 'Below html element is missing v-cloak, v-cloak is used to stop {{ message }} from rendering before the message appears.',
+        message: 'Hello World!'
+    },
+    methods: {
+
+        refresh: function () {
+            window.location.reload();
+        }
+    }
+})
+
+new Vue({
+    el: '#app-2',
+    data: {
+        title: "Below text uses the v-bind directive, which applies reactive behavior to the rendered DOM.",
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+new Vue({
+    el: '#app-3',
+    data: {
+        title: "Below text uses v-if to show text based on a boolean.",
+        seen: true
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-4',
+    data: {
+        title: "Below text uses v-for to list string objects from an array.",
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    },
+    methods: {
+
+        reverseText: function () {
+            this.todos[0].text = this.todos[0].text.split('').reverse().join('')
+        },
+
+        addItem: function () {
+            app5.todos.push({ text: 'New item' })
+        },
+
+        refresh: function () {
+            window.location.reload();
+        }
+    }
+})
+
+new Vue({
+    el: '#app-5',
+    data: {
+        title: "Below examples uses v-on:click for event listening.",
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+new Vue({
+    el: '#app-6',
+    data: {
+        title: "Below example uses v-model to handle input.",
+        message: 'Hello Vue!'
+    }
+})
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+new Vue({
+    el: '#app-7',
+    data: {
+        title: "This uses v-bind to add key numbers to the list items.",
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    }
+})
--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,6 @@
 <template>
   <div id="app">
     <HelloWorld msg="Vue.js Directives"/>
-    <div id="intro">
-      <p>To use Vue.js directives, add this CDN inside the body element of your index.html file.</p>
-      <p>
-        Note: if you want to publish with Vue.js code, it is best to use the
-        <a
-          href="https://cli.vuejs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >vue-cli template.</a>
-      </p>
-      <pre id="intro-code">
-            <code>
- development version of Vue.js:
- &lt;script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"&gt;&lt;/script&gt;
-
- production version of Vue.js:
- &lt;script src="https://cdn.jsdelivr.net/npm/vue"&gt;&lt;/script&gt;
-            </code>
-        </pre>
-      <p style="margin-top: 0; margin-bottom: 0.5em;">
-        For more info, visit the Vue.js site:
-        <a
-          href="https://vuejs.org/v2/guide/installation.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >https://vuejs.org/v2/guide/installation.html</a>
-      </p>
-    </div>
   </div>
 </template>
 
@@ -57,25 +29,5 @@ body {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-#intro {
-  background-color: rgb(78, 235, 235);
-  border: 3px solid black;
-  font-family: "Lucida Console", Monaco, monospace;
-  text-align: center;
-  margin: 1em;
-  padding: 0.5em;
-  left: 0;
-  right: 0;
-  margin-left: auto;
-  margin-right: auto;
-  width: 75%;
-  font-size: 14px;
-}
-
-#intro-code {
-  margin: 0;
-  padding: 0;
 }
 </style>
